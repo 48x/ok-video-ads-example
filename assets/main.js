319e986ca0b40644aedc8f32be6d5a9794daeda5
Maxim Egorichev <maxim.egorichev@corp.mail.ru>
2020-11-02 19:11:50 +0300

    transition to FAPI

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+function prepareRewardedAd() {
+    FAPI.UI.loadAd();
+}
+
+function showRewardedAd() {
+    FAPI.UI.showLoadedAd();
+}
+
+function showInterstitialAd() {
+    FAPI.UI.showLoadedAd();
+}
+
+function getCurrentTime() {
+    var now = new Date();
+    return ("[" + new Date().toLocaleString() + "]");
+}
+
+function clearLog() {
+    document.getElementById("log").textContent = "";
+}
